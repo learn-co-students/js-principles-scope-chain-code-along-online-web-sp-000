28f247113280280d6e54331d8e6de73fabae4d34
Miranda Bonilla <bonillamiranda3@gmail.com>
2019-02-02 23:07:20 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 /* Your code goes here */
+
+var customerName = "bob";
+
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase();
+}
+
+var bestCustomer;
+
+function setBestCustomer() {
+  bestCustomer = "not bob";
+}
+
+function overWriteBestCustomer() {
+  bestCustomer = "maybe bob";
+}
+
+const leastFavoriteCustomer = "flow";
+
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = "joe";
+}
+
+let favoriteCustomer;
+
+function attemptTwoFavoriteCustomers() {
+  let favoriteCustomer = "boss";
+  let favoriteCustomer = "maria";
+}
