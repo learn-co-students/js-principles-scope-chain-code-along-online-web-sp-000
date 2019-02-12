078754e83d9049b150919e9baaba31dc55491db0
Anup Khadka <akhadka.dev@gmail.com>
2019-02-12 19:59:54 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 /* Your code goes here */
+var customerName = "bob";
+
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase();
+  return customerName;
+}
+
+function setBestCustomer() {
+  bestCustomer = 'not bob';
+}
+
+function overWriteBestCustomer() {
+  bestCustomer = 'maybe bob';
+}
+
+const leastFavoriteCustomer = "a";
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = "b";
+}
+
+function attemptTwoFavoriteCustomers() {
+  const favoriteCustomer = "a";
+  const favoriteCustomer = "b";
+}
