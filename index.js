acbd2dcfa5d9f5286a097e58d8e16797be81ea69
Noelle Rolwes <nrolwes@gmail.com>
2019-02-19 11:10:42 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-/* Your code goes here */
+var customerName = 'bob'
+const leastFavoriteCustomer = 'some initial value'
+
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase()
+
+  //notice that there is no return statement
+  //if you try to use `return customerName.toUpperCase()`, it will return 'BOB', but will not redefine the variable!
+}
+
+function setBestCustomer() {
+  bestCustomer = 'not bob'
+}
+
+function overWriteBestCustomer() {
+  bestCustomer = 'maybe bob'
+}
+
+
+
+
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = 'billy'
+}
+
+
+
+
+function attemptTwoFavoriteCustomers(){
+  let favoriteCustomer = "Noelle"
+  let favoriteCustomer = "Alex"
+
+}
