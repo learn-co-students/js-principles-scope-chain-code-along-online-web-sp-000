d52df5822107ad9512a90f61df38ce5ecc6476e4
Cheng Vue <csalvue@gmail.com>
2019-02-23 14:53:22 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 /* Your code goes here */
+var customerName = "bob";
+const leastFavoriteCustomer ="big boy bob";
+
+
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer() {
+  bestCustomer = "not bob";
+}
+
+function overWriteBestCustomer() {
+  bestCustomer = "maybe bob";
+}
+
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer ="bob bob bob";
+}
+
+function attemptTwoFavoriteCustomers() {
+  const leastFavoriteCustomer ="Bob";
+  const leastFavoriteCustomer ="Bobby";
+}
